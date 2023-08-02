0a2f3b801082706931f8b15f3c5c683c4866e4f1
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2023-08-02 15:01:17 +0500

    Adding props to Navbar component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
 function Navbar(props) {
     return(
      <div>
 
      <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">TextUtils</a>
+    <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -19,15 +19,9 @@ function Navbar(props) {
           <a className="nav-link" href="/">Link</a>
         </li>
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+          <a className="nav-link " href="/">
+            {props.about}
           </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
         </li>
         
       </ul>
@@ -42,5 +36,16 @@ function Navbar(props) {
      </div>
     );
 }
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    about: PropTypes.string
+  };
+  
+
+Navbar.defaultProps = {
+    title : "Set the title",
+    about : "About"
+};
 
 export default Navbar;
