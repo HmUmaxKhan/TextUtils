fcf4a8f314c733f89aacbafef841ee0a9b3769ec
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2023-08-03 09:03:35 +0500

    Improving navbar and using useState in the textform

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
-import './App.css';
 import Textform from './Components/Textform';
 
 function App() {
   return (
     
-    <div className="App">
+    <div>
        <Navbar title = "TextUtils" about = "About"/>
        <Textform />
     </div>
--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React , {useState} from "react";
 
 export default function Textform() {
   const height = {
     height: "12rem",
     width: "70rem",
   };
+  const [text,setText]=useState("");
+
+  const handleClick =() => {
+      setText(text.toUpperCase());
+  };
+
+  const handleClick1 =() => {
+    setText(text.toLowerCase());
+};
+
+  const handleClick2 =() => {
+    navigator.clipboard.writeText(text);
+};
+
+
+const handleClick3 =() => {
+    setText(text.replace(/\s+/g," "));
+};
+
+
+const handleClick4 =() => {
+    setText("");
+};
+
+
+
+  const handleChange = (event)=>{
+      setText(event.target.value);
+  }
   return (
     <div>
+     <div className="container">
       <h3 className="my-4">
         <b>Text Area</b>
       </h3>
+      </div>
       <div className="form-floating">
       <div className="container my-4">
         <textarea
@@ -17,10 +48,18 @@ export default function Textform() {
           placeholder="Enter the text here..."
           id="floatingTextarea2"
           style={height}
+          onChange={handleChange}
+          value={text}
         ></textarea>
         </div>
       </div>
-      <button className="btn btn-primary mx-8">Upper Case</button>
+      <div className="container">
+      <button className="btn btn-primary " onClick={handleClick}>Upper Case</button>
+      <button className="btn btn-primary mx-1" onClick={handleClick1}>Lower Case</button>
+      <button className="btn btn-primary mx-1" onClick={handleClick2}>Copy</button>
+      <button className="btn btn-primary mx-1" onClick={handleClick3}>Space Remover</button>
+      <button className="btn btn-primary mx-1" onClick={handleClick4}>Clear</button>
+      </div>
     </div>
   );
 }
